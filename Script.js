a3b17b9f12dd8c7fba1b62d436eace065df0f377
llopisc-bzz <llopisc@bzz.ch>
2023-12-01 22:33:47 +0100

    map on all sites

--- Script.js
+++ Script.js
@@ -9,9 +9,8 @@ function ButtonPos(){
     let posval = map.getBoundingClientRect()
     let countries = ["usa", "de", "jp", "arg"]
     let multiplicatorh = [0.37, 0.29, 0.383, 0.785]
-    let multiplicatorw = [0.175, 0.495, 0.857, 0.316]
+    let multiplicatorw = [0.175, 0.488, 0.84, 0.316]
     let size = posval.width * 0.015
-    let top = posval.top
     for (let i = 0; i < 4; i++) {
         let country = document.getElementById(`${countries[i]}`)
         country.style.top = `${posval.width * 0.5375  * multiplicatorh[i] + posval.top}px`;
